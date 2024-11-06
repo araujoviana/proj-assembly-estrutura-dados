fd564c0547dcae8683f46dc139aec14c09722009
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-06 16:30:50 -0300

    Refactor node to use generic types

--- src/Node.java
+++ src/Node.java
@@ -2,40 +2,62 @@
 // Matheus Gabriel Viana Araujo - 10420444 //
 // Enzo Carvalho Pagliarini - 10425707     //
 /////////////////////////////////////////////
+public class Node<T> {
 
-public class Node {
-	
-	// Parte dos dados do nó.
-	private int value;
-	private char ch;
+	// Valor armazenado no nó
+	private T value;
 
-	// Parte da referência para o próximo nó.
-	private Node next;
-	
-	public Node() { this(-1, '\0'); }
-	public Node(int value, char ch) { this(value, ch, null); }
-	public Node(int value, char ch, Node next) {
+	private Node<T> next; // Próximo nó
+	private Node<T> prev; // Nó anterior
+
+	// REVIEW Múltiplos construtores não parecem ser necessários pro nosso caso
+	////////////////////////////////////////////////
+	// public Node() { //
+	// this(-1); //
+	// } //
+	// //
+	// public Node(int value, Node next) { //
+	// this.value = value; //
+	// this.next = next; //
+	// } //
+	////////////////////////////////////////////////
+
+	public Node(T value) {
 		this.value = value;
-		this.ch = ch;
+		this.next = null;
+		this.prev = null;
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
 		this.next = next;
 	}
-	
-	public int getValue() { return value; }
-	public void setValue(int value) { this.value = value; }
-	
-	public char getChar() { return ch; }
-	public void setChar(char ch) { this.ch = ch; }
-	
-	public Node getNext() { return next; }
-	public void setNext(Node next) { this.next = next; }
-	
-	@Override
-	public String toString() {
-		return super.toString() + " - value: " + value
-				+ ", char: " + ch
-				+ ", next: ("
-				+ (next != null ? (next.getValue() + "," + next.getChar()) : "null")
-				+ ")";
+
+	public Node<T> getPrev() {
+		return prev;
 	}
+
+	public void setPrev(Node<T> prev) {
+		this.prev = prev;
+	}
+   @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + (next != null ? next.getValue() : "null") +
+                ", prev=" + (prev != null ? prev.getValue() : "null") +
+                '}';
+    }
 
 }
